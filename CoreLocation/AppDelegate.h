d2ca7b874dd73cc89713e5cc607a1f26b86991ef
applicationHe <apple@test.local>
2016-04-20 14:35:50 +0800

    Initial Commit

---
+++ CoreLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreLocation
+//
+//  Created by apple on 16/4/20.
+//  Copyright © 2016年 何万牡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreLocation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreLocation
+//
+//  Created by apple on 16/4/20.
+//  Copyright © 2016年 何万牡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
